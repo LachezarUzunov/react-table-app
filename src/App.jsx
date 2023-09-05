91b184dc047ae86695802a7bde8d7180fb1de43a
Lachezar <uzunov_89_lacho@abv.bg>
2023-09-05 14:31:39 +0300

    adding initial files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import tw from 'twin.macro'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        
+    )
+}
+
+export default App
